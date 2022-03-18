1c8d59ce1dec16f78d4f702aba0feecc32ee2314
s-yamauchi07 <hand1nhand306@gmail.com>
2022-03-18 18:10:51 +0900

    applicationコントローラーの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :dog_name, :dog_type_id, :dog_image])
+  end
 end
