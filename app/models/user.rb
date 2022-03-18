4ad867b4c007a0e8d8b566d1ad619ddd3d3c297a
s-yamauchi07 <hand1nhand306@gmail.com>
2022-03-18 18:10:31 +0900

    userモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_one_attached :dog_image
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :dog_type
 end
