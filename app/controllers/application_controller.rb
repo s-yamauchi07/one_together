fb2502e136e22ad1b841862e80321f9066c1fa57
s-yamauchi07 <hand1nhand306@gmail.com>
2022-03-25 10:17:04 +0900

    サインアップ・サインイン後のpath変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    spots_path
+  end
+
+  def after_sign_in_path_for(resource)
+    spots_path
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :dog_name, :dog_type_id, :dog_image])
