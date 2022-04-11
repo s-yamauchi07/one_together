cc01011a5f95d46ebd8c09c89eea2c9718dbe936
s-yamauchi07 <hand1nhand306@gmail.com>
2022-04-11 23:50:41 +0900

    favoriteコントローラーのルーティング生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :spots
+  resources :spots do
+    resources :favorites, only: [:create, :destroy]
+  end
 end
