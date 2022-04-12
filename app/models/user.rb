58fc2900a7f4005ff3ea0f81b02a4fba380ae7b6
s-yamauchi07 <hand1nhand306@gmail.com>
2022-04-12 23:10:19 +0900

    userモデルへアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          
   has_one_attached :dog_image
   has_many :favorites
+  has_many :gone_places
 
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
