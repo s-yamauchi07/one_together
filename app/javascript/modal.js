d6b22dfadcddaa657132e137225e0c8b5ae5599e
s-yamauchi07 <hand1nhand306@gmail.com>
2022-04-15 17:38:00 +0900

    新規投稿画面のmodal修正

--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -1,11 +1,14 @@
 function registrationSpot () {
+  console.log('発火？')
   const registration_btn = document.getElementById('registration')
+  console.log(registration_btn)
   const modal = document.getElementById('registration-form')
-
-  registration_btn.addEventListener('click', modalOpen);
-    function modalOpen() {
+  
+  registration_btn.addEventListener('click', () => {
     modal.style.display = 'block';
-  };
+    const overLay = document.get
+  })
+  
 }
 
 window.addEventListener('load', registrationSpot)
