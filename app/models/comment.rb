0140bd2dc867aa2e17df0f3a53155cccc8645ffd
s-yamauchi07 <hand1nhand306@gmail.com>
2022-04-15 17:33:54 +0900

    コメントモデルの作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,15 @@
 class Comment < ApplicationRecord
+  before_save :default_image
+
   validates :content, presence: true
 
   belongs_to :user
-  belongs_to :comment
+  belongs_to :spot
+  has_one_attached :comment_image 
+
+  def default_image
+    if !self.comment_image.attached?
+      self.comment_image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'no_image.jpg')), filename: 'no_image.jpg', content_type: 'image/jpg')
+    end
+  end
 end
