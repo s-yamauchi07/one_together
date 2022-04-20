703f4c545b9b79062c3f19ca3eeca98b892f4e28
s-yamauchi07 <hand1nhand306@gmail.com>
2022-04-20 23:47:03 +0900

    showアクションへの変数定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # bindiexitng.pry
+    @favorite_spot = @user.favorites
+    @gone_spot = @user.gone_places
   end
 end
