d337c493bf1bbd48d797bf59832f415b4c9da91d
s-yamauchi07 <hand1nhand306@gmail.com>
2022-04-26 23:54:03 +0900

    spotモデルの修正

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,23 +7,6 @@ class Spot < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :spot_image
 
-  with_options presence: true do
-    validates :name
-    validates :phone_number
-    validates :website
-    validates :address
-    validates :latitude
-    validates :longitude
-    validates :spot_image
-  end
-
-  with_options numericality: { other_than: 1 , message: "can't be blank"} do
-    validates :prefecture_id
-    validates :spot_type_id
-    validates :dog_permission_id
-  end
-
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :spot_type
