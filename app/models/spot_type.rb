5fe3bbaea7b04833802a768312477d6185e6f18f
s-yamauchi07 <hand1nhand306@gmail.com>
2022-05-08 16:41:44 +0900

    Active_hashファイルの修正

--- app/models/spot_type.rb
+++ app/models/spot_type.rb
@@ -1,10 +1,9 @@
 class SpotType < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' }, 
-    { id: 2, name: 'レストラン・カフェ', image:'icons/cafe.png' },
-    { id: 3, name: '公園・ドッグラン', image:'icons/park.png' },
-    { id: 4, name: 'ホテル・旅館', image:'icons/hotel.png' },
-    { id: 5, name: 'その他', image:'No image' },
+    { id: 1, name: 'レストラン・カフェ', image:'icons/cafe.png' },
+    { id: 2, name: '公園・ドッグラン', image:'icons/park.png' },
+    { id: 3, name: 'ホテル・旅館', image:'icons/hotel.png' },
+    { id: 4, name: 'その他', image:'No image' },
   ]
 
   include ActiveHash::Associations
