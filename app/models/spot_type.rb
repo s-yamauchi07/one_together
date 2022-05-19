29cd5e3cdc0369f9564829a4ab4ac66c65bef1c7
s-yamauchi07 <hand1nhand306@gmail.com>
2022-05-19 13:37:24 +0900

    Active_hashアイコンの修正

--- app/models/spot_type.rb
+++ app/models/spot_type.rb
@@ -3,7 +3,7 @@ class SpotType < ActiveHash::Base
     { id: 1, name: 'レストラン・カフェ', image:'icons/cafe.png' },
     { id: 2, name: '公園・ドッグラン', image:'icons/park.png' },
     { id: 3, name: 'ホテル・旅館', image:'icons/hotel.png' },
-    { id: 4, name: 'その他', image:'No image' },
+    { id: 4, name: 'その他', image:'icons/no_image.png' },
   ]
 
   include ActiveHash::Associations
