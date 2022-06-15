d9d388039ea95c80d5bc58c49af5879396f6f8d5
s-yamauchi07 <hand1nhand306@gmail.com>
2022-06-15 17:37:51 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :edit, :update, :destroy]
     collection do
       get "search"
+      get "tag_search"
     end
   end
   resources :users, only: [:show]
