73d4523c770f5f9cec61f51738ece7b9446373fe
s-yamauchi07 <hand1nhand306@gmail.com>
2022-07-22 22:09:44 +0900

    is_deletedカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 2022_07_22_060841) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_deleted", default: false
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
