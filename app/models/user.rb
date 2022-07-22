0f1d697c572c3d0489db1d124c25757ad4c53d68
s-yamauchi07 <hand1nhand306@gmail.com>
2022-07-22 22:10:47 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,27 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', on: :create
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :dog_type
+
+  # アプデート時にpasswordが不要になる記述
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
+
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
